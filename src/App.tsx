67cf78dd57abf983dad7581f8441b0f78847b059
Bhavin789 <bhavib.789@gmail.com>
2021-05-08 18:40:01 +0530

    feat: Add intersetion observer API integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,37 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   const [isVisible, setIsVisible] = useState(false);
 
-  const targetRef = useRef(null);
+  const targetRef = useRef<HTMLDivElement | null>(null);
+  const rootRef = useRef(null);
+
+  const options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.5,
+  };
+
+  const observer = new IntersectionObserver((entries: any) => {
+    const [entry] = entries;
+    setIsVisible(entry.isIntersecting);
+  }, options);
+
+  useEffect(() => {
+    if (targetRef.current) {
+      observer.observe(targetRef.current);
+    }
+    return () => {
+      if (targetRef.current) {
+        observer.unobserve(targetRef.current);
+      }
+    };
+  }, [observer]);
 
   return (
-    <div className="root">
+    <div className="root" ref={rootRef}>
       <div className="isVisible">
         {isVisible ? "✅ Visible" : "❌ Not visible"}
       </div>
