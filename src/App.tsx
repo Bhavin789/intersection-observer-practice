3ade9ca23030c66717687ccaab16e30ddc8043f5
Bhavin789 <bhavib.789@gmail.com>
2021-05-08 18:31:38 +0530

    refactor: Add css for target and root elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const targetRef = useRef(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="root">
+      <div className="isVisible">
+        {isVisible ? "✅ Visible" : "❌ Not visible"}
+      </div>
+      <div className="section"></div>
+      <div className="target" ref={targetRef}>
+        target element
+      </div>
     </div>
   );
 }
